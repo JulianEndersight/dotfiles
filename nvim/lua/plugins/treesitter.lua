2b0a19cf2400f5ecd8bffb5cceaa9e9b648ac2cd
Julianendersight <nick.julian.grant@gmail.com>
2025-11-05 00:43:00 -0500

    update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -15,11 +15,19 @@ return {
                     "luadoc", "lua_patterns", "markdown", "markdown_inline",
                     "odin", "query", "vim", "vimdoc", "zig", "rust", "odin"
                 },
+
                 -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
 
                 highlight = {
                     enable = true,
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
                     additional_vim_regex_highlighting = false,
                 }
             })
